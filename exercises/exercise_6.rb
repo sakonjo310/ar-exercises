464b981f9a00c4f2d5ba82506c58344d2d4f880a
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-12 13:35:37 -0800

    Connect two databases and add employee datas

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Satoe", last_name: "Sakonjo", hourly_rate: 70)
+@store1.employees.create(first_name: "Eduardo", last_name: "Vega", hourly_rate: 100)
+@store1.employees.create(first_name: "Kiko", last_name: "Ocampo", hourly_rate: 80)
+@store1.employees.create(first_name: "Tim", last_name: "Quigg", hourly_rate: 120)
+@store2.employees.create(first_name: "Ben", last_name: "Beanie", hourly_rate: 60)
+@store2.employees.create(first_name: "Jae", last_name: "Bacon", hourly_rate: 70)
+@store2.employees.create(first_name: "Russel", last_name: "Winter", hourly_rate: 80)
+@store2.employees.create(first_name: "Andy", last_name: "Andyy", hourly_rate: 90)
+@store2.employees.create(first_name: "Naomi", last_name: "Yamada", hourly_rate: 40)
+@store2.employees.create(first_name: "Whatever", last_name: "What", hourly_rate: 30)
