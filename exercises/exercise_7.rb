60ded399c2b629624051705a1e0365ae09eaf5cb
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-12 14:24:37 -0800

    Add validations and write test data

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Your store name: "
+store_name = gets.chomp
+puts "Annual revenue: "
+store_revenue = gets.chomp
+
+@store7 = Store.create(name: store_name, annual_revenue: store_revenue)
+puts @store7.errors.details
+
+@store2.employees.create(first_name: "Test", hourly_rate: 50)
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
-  
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :carry_apparel
+
+  def carry_apparel
+    if mens_apparel != true && womens_apparel != true
+      errors.add(:apparel, "must carry at least one of the men's or women's apparel")
+    end
+  end
 end
